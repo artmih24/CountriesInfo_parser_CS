e148365a6337dee1c23ef44291aa2576439295ec
artmih24 <83209415+artmih24@users.noreply.github.com>
2021-08-21 15:17:33 +0300

    Add files via upload

---
+++ Test_task/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace Test_task
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.CountryTextBox = new System.Windows.Forms.TextBox();
+            this.DataBaseButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.SortComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AscRadioButton = new System.Windows.Forms.RadioButton();
+            this.DescRadioButton = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.SearchButton);
+            this.groupBox1.Controls.Add(this.CountryTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(378, 54);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Поиск страны по названию";
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SearchButton.Location = new System.Drawing.Point(297, 23);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(75, 23);
+            this.SearchButton.TabIndex = 1;
+            this.SearchButton.Text = "Поиск";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // CountryTextBox
+            // 
+            this.CountryTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CountryTextBox.Location = new System.Drawing.Point(7, 23);
+            this.CountryTextBox.Name = "CountryTextBox";
+            this.CountryTextBox.Size = new System.Drawing.Size(284, 23);
+            this.CountryTextBox.TabIndex = 0;
+            this.CountryTextBox.PlaceholderText = "Введите название страны на английском языке";
+            // 
+            // DataBaseButton
+            // 
+            this.DataBaseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.DataBaseButton.Location = new System.Drawing.Point(396, 20);
+            this.DataBaseButton.Name = "DataBaseButton";
+            this.DataBaseButton.Size = new System.Drawing.Size(112, 47);
+            this.DataBaseButton.TabIndex = 1;
+            this.DataBaseButton.Text = "Вывод всех стран из базы данных";
+            this.DataBaseButton.UseVisualStyleBackColor = true;
+            this.DataBaseButton.Click += new System.EventHandler(this.DataBaseButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 73);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 365);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // SortComboBox
+            // 
+            this.SortComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SortComboBox.FormattingEnabled = true;
+            this.SortComboBox.Items.AddRange(new object[] {
+            "По названию страны",
+            "По коду страны",
+            "По названию столицы",
+            "По площади страны",
+            "По населению страны",
+            "По названию региона"});
+            this.SortComboBox.Location = new System.Drawing.Point(514, 43);
+            this.SortComboBox.Name = "SortComboBox";
+            this.SortComboBox.Size = new System.Drawing.Size(152, 23);
+            this.SortComboBox.TabIndex = 3;
+            this.SortComboBox.SelectedIndexChanged += new System.EventHandler(this.SortComboBox_SelectedIndexChanged_RadioButton_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(514, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Сортировка записей";
+            // 
+            // AscRadioButton
+            // 
+            this.AscRadioButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.AscRadioButton.AutoSize = true;
+            this.AscRadioButton.Location = new System.Drawing.Point(672, 25);
+            this.AscRadioButton.Name = "AscRadioButton";
+            this.AscRadioButton.Size = new System.Drawing.Size(116, 19);
+            this.AscRadioButton.TabIndex = 5;
+            this.AscRadioButton.TabStop = true;
+            this.AscRadioButton.Text = "По возрастанию";
+            this.AscRadioButton.UseVisualStyleBackColor = true;
+            this.AscRadioButton.CheckedChanged += new System.EventHandler(this.SortComboBox_SelectedIndexChanged_RadioButton_CheckedChanged);
+            // 
+            // DescRadioButton
+            // 
+            this.DescRadioButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.DescRadioButton.AutoSize = true;
+            this.DescRadioButton.Location = new System.Drawing.Point(672, 47);
+            this.DescRadioButton.Name = "DescRadioButton";
+            this.DescRadioButton.Size = new System.Drawing.Size(102, 19);
+            this.DescRadioButton.TabIndex = 6;
+            this.DescRadioButton.TabStop = true;
+            this.DescRadioButton.Text = "По убыванию";
+            this.DescRadioButton.UseVisualStyleBackColor = true;
+            this.DescRadioButton.CheckedChanged += new System.EventHandler(this.SortComboBox_SelectedIndexChanged_RadioButton_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DescRadioButton);
+            this.Controls.Add(this.AscRadioButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SortComboBox);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.DataBaseButton);
+            this.Controls.Add(this.groupBox1);
+            this.MinimumSize = new System.Drawing.Size(650, 39);
+            this.Name = "Form1";
+            this.Text = "Тестовое задание Быков А.М.";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox CountryTextBox;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Button DataBaseButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox SortComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton AscRadioButton;
+        private System.Windows.Forms.RadioButton DescRadioButton;
+    }
+}
+
---
+++ Test_task/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Xml;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_task
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class Country
+        {
+            public string name { get; set; }
+            public string alpha3Code { get; set; }
+            public string capital { get; set; }
+            public double area { get; set; }
+            public long population { get; set; }
+            public string region { get; set; }
+        }
+
+        class DB
+        {
+            private static String ConnectionString;// = "Data Source=ARTEM-PC;Initial Catalog=Countries_DB;Integrated Security=True";
+
+            public static DataTable SendQuery(string query)
+            {
+                // чтение XML-файла
+                XmlDocument xmldoc = new XmlDocument();
+                xmldoc.Load("connection.config");
+                // считывание строки подключения
+                foreach (XmlNode node in xmldoc.DocumentElement)
+                {
+                    ConnectionString = node.Attributes[2].Value;
+                }
+                // инициализация объекта класса SqlConnection для подключения к БД
+                SqlConnection connection = new SqlConnection(ConnectionString);
+                DataTable result = new DataTable();
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(result);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    if (connection.State != ConnectionState.Closed)
+                        connection.Close();
+                }
+                return result;
+            }
+        }
+
+        void SetColumnsWidth()
+        {
+            // изменение ширины столбцов
+            DataGridViewColumn namecolumn = dataGridView1.Columns[0];
+            namecolumn.Width = 250; // для названия страны - пошире
+            DataGridViewColumn codecolumn = dataGridView1.Columns[1];
+            codecolumn.Width = 40; // для кода страны можно сделать узкий столбец
+            DataGridViewColumn capitalcolumn = dataGridView1.Columns[2];
+            capitalcolumn.Width = 140; // для названия столицы страны тоже можно сделать столбец пошире
+            DataGridViewColumn regioncolumn = dataGridView1.Columns[5];
+            regioncolumn.Width = 85; // для региона страны можно сделать столбец поуже
+        }
+
+        // поиск по стране
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            SortComboBox.Text = "";
+            if (CountryTextBox.Text != "")
+            {
+                WebClient webclient = new WebClient();
+                try
+                {
+                    // получение данных
+                    var JSON_String = webclient.DownloadString("https://restcountries.eu/rest/v2/name/" + CountryTextBox.Text);
+
+                    // преобразование данных из JSON в тип данных Country
+                    List<Country> countries = JsonSerializer.Deserialize<List<Country>>(JSON_String);
+                    // поскольку возвращается список, содержащий данные об одной стране, берем первую (нулевую) запись
+                    Country country = countries[0];
+
+                    // если в datagridview есть столбцы - удаление столбцов и лишних строк
+                    if (dataGridView1.Columns.Count != 0)
+                    {
+                        dataGridView1.DataSource = null;
+                        dataGridView1.RowCount = 1;
+                        dataGridView1.Columns.Clear();
+                    }
+
+                    // добавление столбцов
+                    dataGridView1.Columns.Add("name", "Название страны");
+                    dataGridView1.Columns.Add("alpha3Code", "Код");
+                    dataGridView1.Columns.Add("capital", "Столица");
+                    dataGridView1.Columns.Add("area", "Площадь");
+                    dataGridView1.Columns.Add("population", "Население");
+                    dataGridView1.Columns.Add("region", "Регион");
+                    SetColumnsWidth();
+
+                    // заполнение ячеек таблицы полученными данными
+                    dataGridView1.Rows[0].Cells["name"].Value = country.name;
+                    dataGridView1.Rows[0].Cells["alpha3Code"].Value = country.alpha3Code;
+                    dataGridView1.Rows[0].Cells["capital"].Value = country.capital;
+                    dataGridView1.Rows[0].Cells["area"].Value = country.area;
+                    dataGridView1.Rows[0].Cells["population"].Value = country.population;
+                    dataGridView1.Rows[0].Cells["region"].Value = country.region;
+
+                    // предложение пользователю сохранить данные в базе данных
+                    DialogResult dialogresult = MessageBox.Show("Сохранить данные в базу данных?", "Сохранение", MessageBoxButtons.YesNo);
+                    // если пользователь хочет сохранить данные в базе данных
+                    if (dialogresult == DialogResult.Yes)
+                    {
+                        // проверка наличия столицы в таблице городов
+                        DataTable CitiesData = DB.SendQuery("SELECT * FROM dbo.Cities WHERE CityName = '" + country.capital + "';");
+                        int CityId = 0;
+                        if (CitiesData.Rows.Count != 0)
+                            // если город есть в таблице, то берем его ID
+                            CityId = Convert.ToInt32(CitiesData.Rows[0].ItemArray[0]);
+                        else
+                        {
+                            // если города нет в таблице, то добавляем его в таблицу
+                            DB.SendQuery("INSERT INTO dbo.Cities (CityName) VALUES ('" + country.capital + "');");
+                            CitiesData = DB.SendQuery("SELECT * FROM dbo.Cities WHERE CityName = '" + country.capital + "';");
+                            CityId = Convert.ToInt32(CitiesData.Rows[0].ItemArray[0]);
+                        }
+                        // проверка наличия регионов в таблице регионов
+                        DataTable RegionsData = DB.SendQuery("SELECT * FROM dbo.Regions WHERE RegionName = '" + country.region + "';");
+                        int RegionId = 0;
+                        if (RegionsData.Rows.Count != 0)
+                            // если регион есть в таблице, берем его номер
+                            RegionId = Convert.ToInt32(RegionsData.Rows[0].ItemArray[0]);
+                        else
+                        {
+                            // если региона нет в таблице, то добавляем его в таблицу
+                            DB.SendQuery("INSERT INTO dbo.Regions (RegionName) VALUES ('" + country.region + "');");
+                            RegionsData = DB.SendQuery("SELECT * FROM dbo.Regions WHERE RegionName = '" + country.region + "';");
+                            RegionId = Convert.ToInt32(RegionsData.Rows[0].ItemArray[0]);
+                        }
+                        // проверка наличия страны в таблице стран
+                        DataTable CountriesData = DB.SendQuery("SELECT * FROM dbo.Countries WHERE CountryCode = '" + country.alpha3Code + "';");
+                        if (CountriesData.Rows.Count == 0)
+                            // если страны нет в таблице - добавляем ее в таблицу
+                           DB.SendQuery("INSERT INTO dbo.Countries (CountryName, CountryCode, CountryCapital, CountryArea, CountryPopulation, CountryRegion) " +
+                                "VALUES ('" + country.name + "', '" + country.alpha3Code + "', " + CityId + ", CONVERT(FLOAT, REPLACE('" + country.area + "', ',', '.')), " + 
+                                country.population + ", " + RegionId + ");");
+                        else
+                            // если страна есть в таблице - обновляем данные о ней
+                            DB.SendQuery("UPDATE dbo.Countries SET CountryName = '" + country.name + "', CountryCapital = " + CityId + ", " +
+                                "CountryArea = CONVERT(FLOAT, REPLACE('" + country.area + "', ',', '.')), CountryPopulation = " + country.population + ", " +
+                                "CountryRegion = " + RegionId + " WHERE CountryCode = '" + country.alpha3Code + "';");
+                    }
+                }
+                catch (WebException webexception)
+                {
+                    WebExceptionStatus status = webexception.Status;
+                    if (status == WebExceptionStatus.ProtocolError)
+                    {
+                        HttpWebResponse httpresponse = (HttpWebResponse)webexception.Response;
+                        MessageBox.Show("Не удалось получить данные.\nОшибка " + (int)httpresponse.StatusCode + " - " + httpresponse.StatusCode);
+                    }
+                    CountryTextBox.Text = "";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Не удалось получить данные.\nОшибка: " + ex.Message);
+                    CountryTextBox.Text = "";
+                }
+            }
+            else
+                MessageBox.Show("Строка поиска страны пустая.");
+        }
+
+        private void DataBaseButton_Click(object sender, EventArgs e)
+        {
+            SortComboBox.Text = "В порядке добавления";
+            dataGridView1.Columns.Clear();
+            DataTable AllCountriesData = DB.SendQuery("SELECT cnt.CountryName AS 'Название страны', cnt.CountryCode AS 'Код', ct.CityName AS 'Столица', " +
+                "cnt.CountryArea AS 'Площадь', cnt.CountryPopulation AS 'Население', r.RegionName AS 'Регион' " +
+                "FROM dbo.Countries cnt INNER JOIN dbo.Cities ct ON cnt.CountryCapital = ct.CityId INNER JOIN dbo.Regions r ON cnt.CountryRegion = r.RegionId;");
+            dataGridView1.DataSource = AllCountriesData;
+            SetColumnsWidth();
+        }
+
+        void SortDataBy(string column_, string order_) {
+            dataGridView1.Columns.Clear();
+            DataTable AllCountriesData = DB.SendQuery("SELECT cnt.CountryName AS 'Название страны', cnt.CountryCode AS 'Код', ct.CityName AS 'Столица', " +
+                "cnt.CountryArea AS 'Площадь', cnt.CountryPopulation AS 'Население', r.RegionName AS 'Регион' " +
+                "FROM dbo.Countries cnt INNER JOIN dbo.Cities ct ON cnt.CountryCapital = ct.CityId INNER JOIN dbo.Regions r ON cnt.CountryRegion = r.RegionId " +
+                "ORDER BY " + column_ + " " + order_  + ";");
+            dataGridView1.DataSource = AllCountriesData;
+            SetColumnsWidth();
+        }
+
+        private void SortComboBox_SelectedIndexChanged_RadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            switch (SortComboBox.SelectedItem)
+            {
+                case "По названию страны":
+                    if (AscRadioButton.Checked == false && DescRadioButton.Checked == false)
+                        AscRadioButton.Checked = true;
+                    if (AscRadioButton.Checked == true)
+                        SortDataBy("cnt.CountryName", "ASC");
+                    else
+                        SortDataBy("cnt.CountryName", "DESC");
+                    break;
+                case "По коду страны":
+                    if (AscRadioButton.Checked == false && DescRadioButton.Checked == false)
+                        AscRadioButton.Checked = true;
+                    if (AscRadioButton.Checked == true)
+                        SortDataBy("cnt.CountryCode", "ASC");
+                    else
+                        SortDataBy("cnt.CountryCode", "DESC");
+                    break;
+                case "По названию столицы":
+                    if (AscRadioButton.Checked == false && DescRadioButton.Checked == false)
+                        AscRadioButton.Checked = true;
+                    if (AscRadioButton.Checked == true)
+                        SortDataBy("ct.CityName", "ASC");
+                    else
+                        SortDataBy("ct.CityName", "DESC");
+                    break;
+                case "По площади страны":
+                    if (AscRadioButton.Checked == false && DescRadioButton.Checked == false)
+                        DescRadioButton.Checked = true;
+                    if (AscRadioButton.Checked == true)
+                        SortDataBy("cnt.CountryArea", "ASC");
+                    else
+                        SortDataBy("cnt.CountryArea", "DESC");
+                    break;
+                case "По населению страны":
+                    if (AscRadioButton.Checked == false && DescRadioButton.Checked == false)
+                        DescRadioButton.Checked = true;
+                    if (AscRadioButton.Checked == true)
+                        SortDataBy("cnt.CountryPopulation", "ASC");
+                    else
+                        SortDataBy("cnt.CountryPopulation", "DESC");
+                    break;
+                case "По названию региона":
+                    if (AscRadioButton.Checked == false && DescRadioButton.Checked == false)
+                        AscRadioButton.Checked = true;
+                    if (AscRadioButton.Checked == true)
+                        SortDataBy("r.RegionName", "ASC");
+                    else
+                        SortDataBy("r.RegionName", "DESC");
+                    break;
+            }
+        }
+    }
+}
